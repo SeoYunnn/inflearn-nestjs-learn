a455eafb1725635473721839aace4cc9aff01a22
seoyun-lee <20212025@o365.hanbat.ac.kr>
2024-02-27 03:35:45 +0900

    chore: eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -120,7 +120,7 @@ module.exports = {
     ],
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "error",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
   },
 };
