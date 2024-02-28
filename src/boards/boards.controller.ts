bc7f3e162ed75250ea3b2cee4b51ce42b6a0337a
seoyun-lee <20212025@o365.hanbat.ac.kr>
2024-02-29 08:19:15 +0900

    Section 3 : CRUD 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,17 +1,49 @@
 import {
-    Controller, Get,
+    Body,
+    Controller, Delete, Get, Param, Patch, Post,
 } from "@nestjs/common";
 import {
     BoardsService,
 } from "./boards.service";
+import {
+    Board, BoardStatus,
+} from "./boards.model";
+import {
+    CreateBoardDto,
+} from "./dto/create-board.dto";
 
 @Controller("boards")
 export class BoardsController {
     constructor(private boardsService: BoardsService) {
     }
 
-    @Get("/users")
-    getAllBoard() {
+    @Get("/")
+    getAllBoard(): Board[] {
         return this.boardsService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto
+    ): Board {
+        return this.boardsService.createBoard(createBoardDto);
+    }
+
+    @Get("/:id")
+    getBoardById(@Param("id") id: string) {
+        return this.boardsService.getBoardByID(id);
+    }
+
+    @Delete("/:id")
+    deleteBoard(@Param("id") id: string): void {
+        this.boardsService.deleteBoard(id);
+    }
+
+    @Patch("/:id/status")
+    updateBoardStatus(
+        @Param("id") id: string,
+        @Body("status") status: BoardStatus,
+    ) {
+        return this.boardsService.updateBoardsStatus(id, status);
     }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,12 +1,53 @@
 import {
     Injectable,
 } from '@nestjs/common';
+import {
+    Board, BoardStatus,
+} from "./boards.model";
+import {
+    v1 as uuid,
+} from "uuid";
+import {
+    CreateBoardDto,
+} from "./dto/create-board.dto";
 
 @Injectable()
 export class BoardsService {
-    private boards = [];
+    private boards: Board[] = [];
 
-    getAllBoards() {
+    getAllBoards(): Board[] {
         return this.boards;
+    }
+
+    createBoard(creteBoardDto:CreateBoardDto) {
+        const {
+            title, description,
+        } = creteBoardDto;
+
+        const board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC,
+        };
+
+        this.boards.push(board);
+
+        return board;
+    }
+
+    getBoardByID(id: string) : Board {
+        return this.boards.find((board) => board.id === id);
+    }
+
+    deleteBoard(id: string): void {
+        this.boards = this.boards.filter((board) => board.id !== id);
+    }
+
+    updateBoardsStatus(id: string, status: BoardStatus): Board {
+        const board: Board | undefined = this.getBoardByID(id);
+        board.status = status;
+
+        return board;
     }
 }
